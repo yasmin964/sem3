630f9c9f0a92494d37f0c85fd040dead05d67470
yasya <kalgexavva1970@icloud.com>
2022-10-21 16:51:32 +0300

    add new text

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
